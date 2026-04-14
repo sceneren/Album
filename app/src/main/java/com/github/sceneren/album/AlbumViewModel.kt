3c206b8a91431ce947ec166105378f265991ea5a
scene <376124750@qq.com>
2026-04-14 16:25:33 +0800

    init

--- app/src/main/java/com/github/sceneren/album/AlbumViewModel.kt
+++ app/src/main/java/com/github/sceneren/album/AlbumViewModel.kt
@@ -1,7 +1,9 @@
 package com.github.sceneren.album
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.sceneren.album.refresh.LoadMoreState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -13,15 +15,54 @@ class AlbumViewModel : ViewModel() {
     private val _imageListFlow = MutableStateFlow<List<ImageItem>>(emptyList())
     val imageListState: StateFlow<List<ImageItem>> get() = _imageListFlow
 
+    private val _loadMoreStateFlow = MutableStateFlow<LoadMoreState>(LoadMoreState.IDLE)
+    val loadMoreState: StateFlow<LoadMoreState> get() = _loadMoreStateFlow
+
+    private val _currentDirFlow = MutableStateFlow<ImageDirectory?>(null)
+    val currentDir: StateFlow<ImageDirectory?> get() = _currentDirFlow
+
+    private val _hasMoreFlow = MutableStateFlow(false)
+    val hasMore: StateFlow<Boolean> get() = _hasMoreFlow
+
+    private var currentPage = 1
+
     fun getImageDirectories() {
         viewModelScope.launch {
             _imageDirectoriesFlow.value = AlbumLoader.getImageDirectories()
         }
     }
 
+    fun setCurrentDir(directory: ImageDirectory?) {
+        _currentDirFlow.value = directory
+        currentPage = 1
+        getImagesByDirectory()
+    }
+
     fun getAllImages() {
         viewModelScope.launch {
             _imageListFlow.value = AlbumLoader.getAllImages(page = 1).data
+        }
+    }
+
+    private fun getImagesByDirectory() {
+        viewModelScope.launch {
+            val result = AlbumLoader.getImagesByDirectory(_currentDirFlow.value?.bucketId ?: ImageDirectory.ALL_BUCKET_ID, page = 1)
+            _imageListFlow.value = result.data
+            _loadMoreStateFlow.value = LoadMoreState.IDLE
+            _hasMoreFlow.value = result.hasNextPage
+        }
+    }
+
+    fun loadMoreImages() {
+        Log.e("AlbumViewModel", "loadMoreImages: ")
+        viewModelScope.launch {
+            _loadMoreStateFlow.value = LoadMoreState.LOADING
+            val bucketId = _currentDirFlow.value?.bucketId ?: ImageDirectory.ALL_BUCKET_ID
+            val result = AlbumLoader.getImagesByDirectory(bucketId, page = ++currentPage)
+            _imageListFlow.value += result.data
+            _loadMoreStateFlow.value = LoadMoreState.IDLE
+            _hasMoreFlow.value = result.hasNextPage
+            Log.e("AlbumViewModel", "loadMoreImages: size = ${result.data.size}, hasNextPage = ${result.hasNextPage}, currentPage = $currentPage, bucketId = $bucketId")
         }
     }
 
--- app/src/main/java/com/github/sceneren/album/MainActivity.kt
+++ app/src/main/java/com/github/sceneren/album/MainActivity.kt
@@ -1,36 +1,49 @@
 package com.github.sceneren.album
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.ImageLoader
+import coil3.compose.AsyncImage
 import coil3.compose.setSingletonImageLoaderFactory
 import coil3.request.crossfade
+import com.github.sceneren.album.refresh.LoadMoreState
+import com.github.sceneren.album.refresh.RefreshLazyVerticalGrid
 import com.github.sceneren.album.ui.theme.AlbumTheme
 import com.hjq.permissions.XXPermissions
 import com.hjq.permissions.permission.PermissionLists
@@ -51,12 +64,22 @@ class MainActivity : ComponentActivity() {
                     val viewModel = viewModel<AlbumViewModel>()
                     val directoryList = viewModel.imageDirectoriesState.collectAsStateWithLifecycle()
                     val imageList = viewModel.imageListState.collectAsStateWithLifecycle()
+                    val selectedDir = viewModel.currentDir.collectAsStateWithLifecycle()
+                    val loadMoreState = viewModel.loadMoreState.collectAsStateWithLifecycle()
+                    val hasMoreData = viewModel.hasMore.collectAsStateWithLifecycle()
                     TestAlbum(
                         innerPadding = innerPadding,
                         dirList = directoryList.value,
                         imageList = imageList.value,
-                        onRequestPermissionSuccess = {},
-                        onRequestPermissionFail = {}
+                        selectedDir = selectedDir.value,
+                        loadMoreState = loadMoreState.value,
+                        hasMoreData = hasMoreData.value,
+                        onLoadMore = viewModel::loadMoreImages,
+                        onRequestPermissionSuccess = viewModel::getImageDirectories,
+                        onRequestPermissionFail = {
+                            Toast.makeText(this, "权限申请失败", Toast.LENGTH_SHORT).show()
+                        },
+                        onChooseDir = viewModel::setCurrentDir
                     )
                 }
             }
@@ -69,15 +92,25 @@ fun TestAlbum(
     innerPadding: PaddingValues,
     dirList: List<ImageDirectory>,
     imageList: List<ImageItem>,
+    selectedDir: ImageDirectory?,
+    loadMoreState: LoadMoreState,
+    hasMoreData: Boolean,
+    onLoadMore: () -> Unit,
     onRequestPermissionSuccess: () -> Unit,
-    onRequestPermissionFail: () -> Unit
+    onRequestPermissionFail: () -> Unit,
+    onChooseDir: (ImageDirectory) -> Unit,
 ) {
 
     val activity = LocalActivity.current
 
     var expanded by remember { mutableStateOf(false) }
 
-    var selectedDir by remember { mutableStateOf<ImageDirectory?>(null) }
+    val lazyGridState = rememberLazyGridState()
+
+    // 切换目录后，将图片列表滚动到顶部
+    LaunchedEffect(selectedDir) {
+        lazyGridState.scrollToItem(0)
+    }
 
     Column(modifier = Modifier.padding(innerPadding)) {
         Button(onClick = {
@@ -94,6 +127,7 @@ fun TestAlbum(
                 }
         }) { Text("申请权限") }
 
+        // 目录
         Box {
             Text(
                 modifier = Modifier
@@ -115,14 +149,53 @@ fun TestAlbum(
                     DropdownMenuItem(
                         text = { Text(text = it.bucketName) },
                         onClick = {
-                            selectedDir = it
                             expanded = false
+                            onChooseDir(it)
                         }
                     )
                 }
             }
         }
+        // 图片列表
+        RefreshLazyVerticalGrid(
+            columns = GridCells.Fixed(4),
+            lazyGridState = lazyGridState,
+            loadMoreState = loadMoreState,
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(8.dp),
+            hasMoreData = hasMoreData,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            onLoadMore = onLoadMore
+        ) {
+            items(imageList) { image ->
+                ImageItemView(image)
+            }
+        }
 
+    }
+}
 
+@Composable
+fun ImageItemView(image: ImageItem) {
+    Card(modifier = Modifier.fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                model = image.uri,
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = image.displayName,
+            )
+
+        }
     }
 }
